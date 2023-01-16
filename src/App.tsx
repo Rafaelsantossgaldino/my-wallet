aa00e91d9892037b22c4a38f5364963155b2ca2c
rafael <rafaelsantossgaldino2@gmail.com>
2023-01-16 13:36:57 -0300

    config fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 import React from "react";
+import AppLoading from "expo-app-loading";
 import { View, Text } from "react-native";
 
+import {
+  useFonts,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  Poppins_800ExtraBold
+ } from "@expo-google-fonts/poppins";
+
+ import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
+ import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
+
+
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular,
+  })
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
   return (
-    <View>
-      <Text>My Wallet</Text>
+    <View
+        style={{
+          flex:1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+      <Text>My Wallett</Text>
     </View>
   );
 };
