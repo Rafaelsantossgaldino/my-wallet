b6e39592a9305ccc75341724c0ad7d858ec39c06
rafael <rafaelsantossgaldino2@gmail.com>
2023-01-18 10:18:05 -0300

    criando components

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { Text } from "react-native";
+import { SafeAreaView } from "react-native";
 
-import { Container } from "./styles";
+import { Container, ContentHeader, Title, Description, ViewButton,  ContentBody, ContentFooter} from "./styles";
 
 const Login: React.FC = () => {
   return (
-    <Container>
-      <Text>Screen Login</Text>
-    </Container>
-  );
-}
-export { Login };
+    <SafeAreaView>
+      <Container>
+        <ContentHeader>
+          <Title>Seja Bem vindo(a) {"\n"} ao RS WalleApp</Title>
 
+          <Description>Entrar com rede sociais</Description>
+
+          <ViewButton></ViewButton>
+        </ContentHeader>
+
+        <ContentBody>
+
+        </ContentBody>
+
+        <ContentFooter>
+
+        </ContentFooter>
+      </Container>
+    </SafeAreaView>
+  );
+};
+export { Login };
