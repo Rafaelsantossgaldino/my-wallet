343fdb3185bda5cda1d62d9e69cf825b228f50a9
rafael <rafaelsantossgaldino2@gmail.com>
2023-01-18 11:43:56 -0300

    estilo do botao do google

---
+++ src/components/ButtonSocialGoogle/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components/native";
+import { RectButton } from "react-native-gesture-handler";
+import { RFValue } from "react-native-responsive-fontsize";
+import theme from "../../styles/theme";
+
+export const Button = styled(RectButton)`
+  align-items: center;
+  flex-direction: row;
+  margin-bottom: 16px;
+  justify-content: center;
+  border-radius: ${RFValue(5)}px;
+  width: ${RFValue(130)}px;
+  height: ${RFValue(60)}px;
+  margin-left: ${RFValue(10)}px;
+  box-shadow: 1px 3px 3px rgba(0,0,0,0.2);
+  background-color: ${({ theme }) => theme.COLORS.GRAY6};
+`;
+
+export const IconeGoogle = styled.Image`
+  width: ${RFValue(25)}px;
+  height: ${RFValue(25)}px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(17)}px;
+  margin-left: ${RFValue(10)}px;
+  font-family: ${({ theme }) => theme.FONTS.POPPINSLIGHT};
+`;
