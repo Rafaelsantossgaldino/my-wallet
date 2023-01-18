f3084313d25b7b96e142b59c443105bc535f881b
rafael <rafaelsantossgaldino2@gmail.com>
2023-01-18 09:18:31 -0300

    add themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import AppLoading from "expo-app-loading";
-import { View, Text } from "react-native";
+import { View, ActivityIndicator, Text } from "react-native";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import COLORS from "../src/styles/theme";
 
 import {
   useFonts,
@@ -9,11 +11,10 @@ import {
   Poppins_500Medium,
   Poppins_700Bold,
   Poppins_800ExtraBold
- } from "@expo-google-fonts/poppins";
+} from "@expo-google-fonts/poppins";
 
- import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
- import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
-
+import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
+import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
 
 const App: React.FC = () => {
   const [fontsLoaded] = useFonts({
@@ -24,23 +25,25 @@ const App: React.FC = () => {
     Poppins_800ExtraBold,
     DMSans_400Regular,
     DMSerifDisplay_400Regular,
-  })
+  });
 
   if(!fontsLoaded){
-    return <AppLoading />
-  }
+    return <ActivityIndicator />
+  };
 
   return (
-    <View
+    <ThemeProvider theme={COLORS}>
+      
+      <View
         style={{
           flex:1,
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-      <Text>My Wallett</Text>
-    </View>
+        <Text>My Wallett</Text>
+      </View>
+    </ThemeProvider>
   );
 };
-
 export default App;
