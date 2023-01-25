4086f5748f910ee8b6017ae5c5d2a5f333cab22d
rafael <rafaelsantossgaldino2@gmail.com>
2023-01-25 14:52:52 -0300

    adicionando placeholder nos input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,35 +1,40 @@
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { useTheme } from "styled-components";
+import { TextInputProps } from "react-native";
 import { Container, InputContainer } from "./styles";
 
 interface InputProps {
   RightIcon?: boolean;
   LeftIcon?: boolean;
+  iconName: string;
+  iconSize?: number;
+  iconColor?: string;
 }
 
-const Input: React.FC<InputProps> = ({RightIcon, LeftIcon}) => {
+const Input: React.FC<InputProps & TextInputProps> = ({RightIcon, LeftIcon, iconName, iconSize, iconColor, ...rest }) => {
   const { COLORS } =  useTheme();
 
   return (
     <Container>
       {LeftIcon && (
         <Ionicons
-          name="mail-outline"
-          size={20}
-          color={COLORS.TEXTDARK}
-          style={{padding: 5, marginLeft: 10}}
-
+          name={iconName}
+          size={iconSize}
+          color={ iconColor || COLORS.TEXTDARK}
+          style={{padding: 5 }}
         />
       )}
-      <InputContainer />
+      <InputContainer
+        {...rest}
+        placeholderTextColor={COLORS.GRAY3}
+      />
       {RightIcon && (
         <Ionicons
-        name="lock-closed-outline"
-        size={20}
-        color={COLORS.TEXTDARK}
-        style={{padding: 5, marginLeft: 10}}
-
+        name={iconName}
+        size={iconSize}
+        color={ iconColor || COLORS.TEXTDARK}
+        style={{padding: 5 }}
         />
       )}
     </Container>
--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { SafeAreaView, Text } from "react-native";
 
 import { Input } from "../../components/Input/Input";
+import { useTheme } from "styled-components";
 import { ButtonSocialGoogle } from "../../components/ButtonSocialGoogle/ButtonSocialGoogle";
 import { ButtonSocial } from "../../components/ButtonSocial/ButtonSocial";
 import { Container, ContentHeader, Title, Description, ViewButton,  ContentBody, ContentFooter} from "./styles";
 
 
 const Login: React.FC = () => {
+  const { COLORS } = useTheme();
+
   return (
     <SafeAreaView>
       <Container>
@@ -23,8 +26,19 @@ const Login: React.FC = () => {
         </ContentHeader>
 
         <ContentBody>
-          <Input LeftIcon />
-          <Input RightIcon />
+          <Input
+            LeftIcon
+            iconSize={25}
+            iconName="mail-outline"
+            placeholder="Digite seu email"
+
+            />
+          <Input
+            LeftIcon
+            iconSize={25}
+            iconName="lock-closed-outline"
+            placeholder="Digite sua senha"
+          />
         </ContentBody>
 
         <ContentFooter>
